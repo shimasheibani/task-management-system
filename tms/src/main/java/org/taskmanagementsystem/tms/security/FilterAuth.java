bb0418414dc33c7669aebaf91233ba6b5bd24b32
shima <shima.sheibani72@gmail.com>
2026-08-07 13:29:50 +0200

    Completing security Config

--- tms/src/main/java/org/taskmanagementsystem/tms/security/FilterAuth.java
+++ tms/src/main/java/org/taskmanagementsystem/tms/security/FilterAuth.java
@@ -1,43 +1,52 @@
-//package org.taskmanagementsystem.tms.security;
-//
-//import ch.qos.logback.core.util.StringUtil;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.AllArgsConstructor;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.stereotype.Component;
-//import org.springframework.util.StringUtils;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import java.io.IOException;
-//
-//@Component
-//@Slf4j
-//@RequiredArgsConstructor
-//public class FilterAuth extends OncePerRequestFilter {
-//    private final JwtUtilis jwtUtilis;
-//    private final CustomUserDetailsService customUserDetailsService;
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        String token  = getTokenFromRequest(request);
-//        if(token != null){
-//            String email = jwtUtilis.getUsernameFromToken(token);
-//            UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
-//            if (StringUtils.hasText(email) && jwtUtilis.isTokenValid(token, userDetails))
-//        }
-//    }
-//
-//    public String getTokenFromRequest(HttpServletRequest request){
-//        String tokenHeader = request.getHeader("Authorization");
-//        if(tokenHeader != null && tokenHeader.startsWith("Bearer ")){
-//            return tokenHeader.substring(7);
-//        }
-//        return null;
-//
-//    }
-//
-//}
+package org.taskmanagementsystem.tms.security;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class FilterAuth extends OncePerRequestFilter {
+    private final JwtUtilis jwtUtilis;
+    private final CustomUserDetailsService customUserDetailsService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token  = getTokenFromRequest(request);
+        if(token != null){
+            try{
+                String email = jwtUtilis.getUsernameFromToken(token);
+                UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+                if (StringUtils.hasText(email) && jwtUtilis.isTokenValid(token, userDetails)){
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    }
+            }
+            catch (Exception e) {
+                SecurityContextHolder.clearContext();
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    public String getTokenFromRequest(HttpServletRequest request){
+        String tokenHeader = request.getHeader("Authorization");
+        if(tokenHeader != null && tokenHeader.startsWith("Bearer ")){
+            return tokenHeader.substring(7);
+        }
+        return null;
+    }
+
+}
